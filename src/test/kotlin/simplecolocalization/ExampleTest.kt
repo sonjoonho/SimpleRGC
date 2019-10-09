037dfbcec367cccbc0505196cdb8faedbdc70559
Joon-Ho Son <sonjoonh@gmail.com>
2019-10-09 19:41:35 +0100

    Initial boilerplate and Maven project setup.

---
+++ src/test/kotlin/simplecolocalization/ExampleTest.kt
@@ -0,0 +1,11 @@
+package simplecolocalization
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ExampleTest {
+    @Test
+    fun twoPlusTwo() {
+        assertEquals(4, (2 + 2).toLong())
+    }
+}
