2f98d8e1b153500c79d4c7050050b734f83a60b2
Kelvin Zhang <me@iamkelv.in>
2019-10-13 16:06:28 +0100

    Implement a dialog and results box [kcz17]

--- src/main/kotlin/simplecolocalization/SimpleColocalization.kt
+++ src/main/kotlin/simplecolocalization/SimpleColocalization.kt
@@ -1,46 +1,114 @@
 package simplecolocalization
 
 import ij.ImagePlus
-import java.io.File
 import net.imagej.ImageJ
+import org.scijava.ItemVisibility
 import org.scijava.command.Command
 import org.scijava.plugin.Parameter
 import org.scijava.plugin.Plugin
+import org.scijava.table.DefaultGenericTable
+import org.scijava.table.IntColumn
+import org.scijava.ui.UIService
+import org.scijava.widget.NumberWidget
+import java.io.File
 
 /**
- * This provides basic scaffolding for an ImageJ plugin.
+ * Segments and counts cells which are almost circular in shape which are likely
+ * to overlap.
  *
+ * When this plugin is started in a graphical context (as opposed to the command
+ * line), a dialog box is opened with the script parameters below as input.
+ *
+ * [run] contains the main pipeline, which runs only after the script parameters
+ * are populated.
  */
 @Plugin(type = Command::class, menuPath = "Plugins > Simple Colocalization")
 class SimpleColocalization : Command {
 
+    /**
+     * Entry point for UI operations, automatically handling both graphical and
+     * headless use of this plugin.
+     */
     @Parameter
+    private lateinit var uiService: UIService
+
+    /** File path of the input image. */
+    @Parameter(label = "Input Image")
     private lateinit var imageFile: File
 
+    @Parameter(
+        label = "Preprocessing Parameters:",
+        visibility = ItemVisibility.MESSAGE,
+        required = false
+    )
+    private lateinit var preprocessingParamsHeader: String
+
+    /**
+     * Applied to the input image to reduce sensitivity of the thresholding
+     * algorithm. Higher value means more blur.
+     */
+    @Parameter(
+        label = "Gaussian Blur Sigma (Radius)",
+        description = "Reduces sensitivity to cell edges by blurring the " +
+            "overall image. Higher is less sensitive.",
+        min = "0.0",
+        stepSize = "1.0",
+        style = NumberWidget.SPINNER_STYLE,
+        required = true,
+        persist = false
+    )
+    private var gaussianBlurSigma = 3.0F
+
+    @Parameter(
+        label = "Cell Identification Parameters:",
+        visibility = ItemVisibility.MESSAGE,
+        required = false
+    )
+    private lateinit var identificationParamsHeader: String
+
+    /**
+     * Used during the cell identification stage to reduce overlapping cells
+     * being grouped into a single cell.
+     */
+    @Parameter(
+        label = "Largest Cell Diameter (µm)",
+        min = "5.0",
+        stepSize = "1.0",
+        style = NumberWidget.SPINNER_STYLE,
+        required = true,
+        persist = false
+    )
+    private var largestCellDiameter = 30.0F
+
+    /** Displays the resulting count as a results table. */
+    fun showCount(count: Int) {
+        val table = DefaultGenericTable()
+        val countColumn = IntColumn()
+        countColumn.add(count)
+        table.add(countColumn)
+        table.setColumnHeader(0, "Count")
+        uiService.show(table)
+    }
+
+    /** Runs after the parameters above are populated. */
     override fun run() {
         val image = ImagePlus(imageFile.absolutePath)
         image.show()
         image.channelProcessor.autoThreshold()
-        image.show()
+        showCount(1)
     }
 
     companion object {
-
         /**
-         * This main function serves for development purposes.
-         * It allows you to run the plugin immediately out of
-         * your integrated development environment (IDE).
-         * @param args whatever, it's ignored
-         * *
+         * Entry point to directly open the plugin, used for debugging purposes.
+         *
          * @throws Exception
          */
         @Throws(Exception::class)
         @JvmStatic
         fun main(args: Array<String>) {
-            // create the ImageJ application context with all available services
             val ij = ImageJ()
             ij.ui().showUI()
-
             ij.command().run(SimpleColocalization::class.java, true)
         }
     }
