8d47a6164fdf533713b03967bc3289b9c70747be
Kelvin Zhang <me@iamkelv.in>
2019-10-19 17:54:33 +0100

    Implement opening of channel analysis in results table

--- src/main/kotlin/simplecolocalization/SimpleColocalization.kt
+++ src/main/kotlin/simplecolocalization/SimpleColocalization.kt
@@ -35,6 +35,8 @@ import java.io.File
 @Plugin(type = Command::class, menuPath = "Plugins > Simple Colocalization")
 class SimpleColocalization : Command {
 
+    private val numRgbChannels = 3
+
     /**
      * Entry point for UI operations, automatically handling both graphical and
      * headless use of this plugin.
@@ -149,13 +151,18 @@ class SimpleColocalization : Command {
     override fun run() {
         val originalImage = ImagePlus(imageFile.absolutePath)
         val image = ImagePlus(imageFile.absolutePath)
+
         preprocessImage(image)
         segmentImage(image)
+
         val roiManager = RoiManager.getRoiManager()
         val cells = identifyCells(roiManager, image)
         markCells(originalImage, cells)
-        analyseCells(originalImage, cells)
+
+        val analysis = analyseCells(originalImage, cells)
+
         showCount(cells.size)
+        showCellAnalysis(analysis)
         originalImage.show()
     }
 
@@ -192,39 +199,92 @@ class SimpleColocalization : Command {
     /**
      * Analyses the RGB intensity of the cells.
      */
-    private fun analyseCells(image: ImagePlus, cells: Array<Roi>): Array<CellAnalysis> {
+    private fun analyseCells(image: ImagePlus, highlightedCells: Array<Roi>): Array<CellAnalysis> {
         // Convert to RGB so that we can get the r,g,b for each pixel
         ImageConverter(image).convertToRGB()
 
         // Image will have 3 channels as it is RGB following conversion
-        val nChannels = 3
         val analyses = arrayListOf<CellAnalysis>()
-        for (cell in cells) {
+        for (cell in highlightedCells) {
             var area = 0
-            val sums = MutableList(nChannels) { 0 }
-            val mins = MutableList(nChannels) { Integer.MAX_VALUE }
-            val maxs = MutableList(nChannels) { Integer.MIN_VALUE }
+            val sums = MutableList(numRgbChannels) { 0 }
+            val mins = MutableList(numRgbChannels) { Integer.MAX_VALUE }
+            val maxs = MutableList(numRgbChannels) { Integer.MIN_VALUE }
             val containedCells = cell.containedPoints
             containedCells.forEach { point ->
                 // pixelData is of the form [r, g, b, 0]
                 val pixelData = image.getPixel(point.x, point.y)
                 area++
-                for (channel in 0 until nChannels) {
+                for (channel in 0 until numRgbChannels) {
                     sums[channel] += pixelData[channel]
                     mins[channel] = Integer.min(mins[channel], pixelData[channel])
                     maxs[channel] = Integer.max(maxs[channel], pixelData[channel])
                 }
             }
             val channels = mutableListOf<ChannelAnalysis>()
-            for (channel in 0 until nChannels) {
+            for (channel in 0 until numRgbChannels) {
                 channels.add(ChannelAnalysis(sums[channel] / area, mins[channel], maxs[channel]))
             }
             analyses.add(CellAnalysis(area, channels))
         }
-        // Revert to the initial channel number.
 
         return analyses.toTypedArray()
     }
+
+    /** Displays the resulting cell analysis as a results table. */
+    private fun showCellAnalysis(analyses: Array<CellAnalysis>) {
+        val table = DefaultGenericTable()
+
+        val areaColumn = IntColumn()
+        val redMeanColumn = IntColumn()
+        val redMinColumn = IntColumn()
+        val redMaxColumn = IntColumn()
+        val greenMeanColumn = IntColumn()
+        val greenMinColumn = IntColumn()
+        val greenMaxColumn = IntColumn()
+        val blueMeanColumn = IntColumn()
+        val blueMinColumn = IntColumn()
+        val blueMaxColumn = IntColumn()
+
+        analyses.forEachIndexed { index, channelAnalysis ->
+            areaColumn.add(channelAnalysis.area)
+
+            redMeanColumn.add(channelAnalysis.channels[0].mean)
+            redMinColumn.add(channelAnalysis.channels[0].min)
+            redMaxColumn.add(channelAnalysis.channels[0].max)
+            blueMeanColumn.add(channelAnalysis.channels[1].mean)
+            blueMinColumn.add(channelAnalysis.channels[1].min)
+            blueMaxColumn.add(channelAnalysis.channels[1].max)
+            greenMeanColumn.add(channelAnalysis.channels[2].mean)
+            greenMinColumn.add(channelAnalysis.channels[2].min)
+            greenMaxColumn.add(channelAnalysis.channels[2].max)
+        }
+
+        table.add(areaColumn)
+        table.add(redMeanColumn)
+        table.add(redMinColumn)
+        table.add(redMaxColumn)
+        table.add(greenMeanColumn)
+        table.add(greenMinColumn)
+        table.add(greenMaxColumn)
+        table.add(blueMeanColumn)
+        table.add(blueMinColumn)
+        table.add(blueMaxColumn)
+
+        table.setColumnHeader(0, "Area")
+        table.setColumnHeader(1, "Red Mean")
+        table.setColumnHeader(2, "Red Min")
+        table.setColumnHeader(3, "Red Max")
+        table.setColumnHeader(4, "Green Mean")
+        table.setColumnHeader(5, "Green Min")
+        table.setColumnHeader(6, "Green Max")
+        table.setColumnHeader(7, "Blue Mean")
+        table.setColumnHeader(8, "Blue Min")
+        table.setColumnHeader(9, "Blue Max")
+
+        uiService.show(table)
+    }
+
 
     companion object {
         /**
