e307859e9e9ef02d42199c59802a1ab0d9ceb316
Will Burr <williamburr98@gmail.com>
2019-11-11 17:34:53 +0000

    Rearrange imports

--- src/main/kotlin/simplecolocalization/services/cellcomparator/DistanceCellComparator.kt
+++ src/main/kotlin/simplecolocalization/services/cellcomparator/DistanceCellComparator.kt
@@ -1,8 +1,8 @@
 package simplecolocalization.services.cellcomparator
 
-import simplecolocalization.services.colocalizer.PositionedCell
 import kotlin.math.pow
 import kotlin.math.sqrt
+import simplecolocalization.services.colocalizer.PositionedCell
 
 /**
  * Determines whether two cells overlap based on the Euclidean distance between the
