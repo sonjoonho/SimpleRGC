f59424d855eb5d359b36f6deb582639d4a84dd8c
Joon-Ho Son <sonjoonh@gmail.com>
2019-11-21 14:41:27 +0000

    Initial implementation of show

--- src/main/kotlin/simplecolocalization/SimpleCellManager.kt
+++ src/main/kotlin/simplecolocalization/SimpleCellManager.kt
@@ -1,6 +1,9 @@
 package simplecolocalization
 
 import ij.ImagePlus
+import ij.gui.Overlay
+import ij.gui.Roi
+import ij.plugin.OverlayLabels
 import simplecolocalization.services.colocalizer.PositionedCell
 
 class SimpleCellManager {
@@ -15,6 +18,11 @@ class SimpleCellManager {
     }
 
     fun show(imp: ImagePlus) {
-        TODO("Unimplemented")
+        val rois: Collection<Roi> = cells.map { it.toRoi() }
+        val overlay: Overlay = OverlayLabels.createOverlay()
+        rois.forEach { overlay.add(it) }
+        val ic = imp.canvas
+        ic.showAllList = overlay
+        imp.draw()
     }
 }
