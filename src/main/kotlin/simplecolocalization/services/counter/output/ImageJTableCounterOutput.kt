1fde518ab46b3bd80855651d16447a2c63b41630
Arjun Singh <arjunsingh@dyn3210-228.wlan.ic.ac.uk>
2019-11-29 16:56:03 +0000

    Improve ImageJTableCounterOutput implementation.

--- src/main/kotlin/simplecolocalization/services/counter/output/ImageJTableCounterOutput.kt
+++ src/main/kotlin/simplecolocalization/services/counter/output/ImageJTableCounterOutput.kt
@@ -1,15 +1,25 @@
 package simplecolocalization.services.counter.output
 
+import org.scijava.table.CharColumn
+import org.scijava.table.Column
 import org.scijava.table.DefaultGenericTable
 import org.scijava.table.IntColumn
 import org.scijava.ui.UIService
 
-class ImageJTableCounterOutput(private val count: Int, private val uiService: UIService) : CounterOutput() {
+class ImageJTableCounterOutput(private val uiService: UIService) : CounterOutput() {
 
-    override fun output() {
+    private val table: DefaultGenericTable = DefaultGenericTable()
+    private val countColumn: IntColumn = IntColumn()
+
+    override fun addCountForFile(count: Int, file: String) {
+        countColumn.add(count)
+
+        // TODO: Implement storing and displaying filenames
+    }
+
+    fun show() {
         val table = DefaultGenericTable()
         val countColumn = IntColumn()
-        countColumn.add(count)
         table.add(countColumn)
         table.setColumnHeader(0, "Count")
         uiService.show(table)
