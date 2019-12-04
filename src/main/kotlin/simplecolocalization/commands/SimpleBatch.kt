ce42dff9a1a26cb3cfe55dcbe41a1151d0e8dbd6
Tiger Cross <tc2017@ic.ac.uk>
2019-12-04 12:25:13 +0000

    Implement calling of lif processing macro and check for bio-formats.

--- src/main/kotlin/simplecolocalization/commands/SimpleBatch.kt
+++ src/main/kotlin/simplecolocalization/commands/SimpleBatch.kt
@@ -5,6 +5,7 @@ import ij.gui.GenericDialog
 import ij.gui.MessageDialog
 import ij.io.DirectoryChooser
 import java.io.File
+import java.nio.file.Paths
 import net.imagej.ImageJ
 import org.scijava.Context
 import org.scijava.command.Command
@@ -108,26 +109,50 @@ class SimpleBatch : Command {
 
         val files = getAllFiles(file, shouldProcessFilesInNestedFolders)
 
+        val tifs = files.filter { it.extension == "tif" || it.extension == "tiff" }.toMutableList()
+
         val lifs = files.filter { it.extension == "lif" }
+
         if (lifs.isNotEmpty()) {
 
-            val dialog = GenericDialog(".LIF files found")
+            // Check if bioformats is installed.
+            val allCommands = ij.Menus.getCommands().keys().toList()
 
-            dialog.addMessage("We found ${lifs.size} files with the .LIF extension. \n" +
-                "Please note that this plugin will skip over files in the .LIF format. \n" +
-                "Please refer to this plugin's documentation on how to automatically " +
-                "batch convert .LIF files to the accepted .TIF extension.")
+            if (allCommands.contains("Bio-Formats")) {
+                val currDir = Paths.get("").toAbsolutePath().toString()
+                val tmp = createTempDir()
+                println(tmp)
+                for (lif in lifs) {
+                    // Pass in args: LifPath|outputPath
+                    val args = lif.toString() + "|" + currDir + tmp.toString()
+                    IJ.runMacroFile(currDir + "/scripts/process_lif_macro.ijm", args)
 
-            dialog.addMessage("Continue to process only .TIF images in your input directory")
+                    // I think we can do what the macro does programatically with the following:
+                    // val pluginResult = IJ.runPlugIn("Bio-Formats Importer", "open=[" + lif + "] color_mode=Composite rois_import=[ROI manager] open_all_series view=Hyperstack stack_order=XYCZT")
+                    // if (pluginResult == null) {
+                    //     print("Bioformats not detected");
+                    // }
+                }
+                // Add all files in created temp folder to tiffs.
+                tifs.addAll(tmp.listFiles())
+            } else {
+                val dialog = GenericDialog("LIF file warning")
 
-            dialog.showDialog()
+                dialog.addMessage("We found ${lifs.size} files with the .lif extension, which require installation of " +
+                    "the Bio-Formats plugin to open.\n" +
+                    "We have detected that the Bio-Formats Plugin is not installed in your version of ImageJ.\n" +
+                    "Please install Bio-Formats and run the plugin again if you would like to process LIF files.\n" +
+                    "Details on how to install the Bio-Formats plugin can be found at: https://docs.openmicroscopy.org/bio-formats/5.8.2/users/imagej/installing.html")
 
-            if (dialog.wasCanceled()) {
-                return
+                dialog.addMessage("select OK to process only .tif files. Otherwise, select Cancel to abort.")
+
+                dialog.showDialog()
+
+                if (dialog.wasCanceled()) {
+                    return
+                }
             }
         }
-
-        val tifs = files.filter { it.extension == "tif" || it.extension == "tiff" }
 
         process(tifs, outputPath)
     }
