0d776c8265189d5aafbdb034712421aab3d4a49b
Rasika Navarange <rn3717@ic.ac.uk>
2020-06-07 16:30:55 +0100

    Basic Simple Colocalization

--- src/main/kotlin/simplecolocalization/commands/batch/SimpleBatch.kt
+++ src/main/kotlin/simplecolocalization/commands/batch/SimpleBatch.kt
@@ -140,7 +140,7 @@ class SimpleBatch : Command {
 
     /** Creates the Simple Cell Counter GUI. */
     private fun simpleCellCounterPanel(): JPanel {
-        // TODO: Make this pretty, add a layout manager
+        // TODO: Make this pretty
         // TODO: Make User parameters persist
         val panel = JPanel()
         panel.layout = GridLayout(0, 1)
@@ -175,9 +175,6 @@ class SimpleBatch : Command {
 
         val gaussianBlurModel = SpinnerNumberModel(3, 1, 50, 1)
         val gaussianBlurSpinner = addSpinner(panel, "Gaussian blur sigma", gaussianBlurModel)
-
-        // TODO: Make removeAxons actually do something
-        val removeAxonsCheckbox = addCheckBox(panel, "Remove Axons")
 
         val outputParamsLabel = JLabel("Output parameters")
         panel.add(outputParamsLabel)
@@ -219,10 +216,8 @@ class SimpleBatch : Command {
                     ""
             }
 
-            val shouldProcessFilesInNestedFolders = shouldProcessFilesInNestedFoldersCheckbox.isSelected
-
             // TODO: handle the null inputFolder correctly
-            val files = getAllFiles(inputFolder!!, shouldProcessFilesInNestedFolders)
+            val files = getAllFiles(inputFolder!!, shouldProcessFilesInNestedFoldersCheckbox.isSelected)
 
             val cellCounter = BatchableCellCounter(channel, context)
 
@@ -240,8 +235,107 @@ class SimpleBatch : Command {
 
     /** Creates the Simple Colocalizer GUI. */
     private fun simpleColocalizerPanel(): JPanel {
-        // TODO: Create panel for Simple Colocalizer
-        return JPanel()
+        // TODO: Make this pretty
+        val panel = JPanel()
+        panel.layout = GridLayout(0, 1)
+
+        // TODO: Handle no directory chosen (when inputFolder is null)
+        var inputFolder: File? = null
+        val button = addFileChooser(panel, "Input folder")
+        button.addActionListener {
+            val fileChooser = JFileChooser()
+            fileChooser.fileSelectionMode = JFileChooser.DIRECTORIES_ONLY
+            val i = fileChooser.showOpenDialog(panel)
+            if (i == JFileChooser.APPROVE_OPTION) {
+                inputFolder = fileChooser.selectedFile
+            }
+        }
+
+        val shouldProcessFilesInNestedFoldersCheckbox = addCheckBox(panel, "Batch process in nested sub-folders?")
+
+        val colocalizationInstructionLabel = JLabel("<html><div align=\"right\">\nWhen performing batch colocalization, ensure that <br />all input images have the same channel ordering as<br />specified below.</div></html>")
+        panel.add(colocalizationInstructionLabel)
+
+        val channel1Model = SpinnerNumberModel(1, 1, 100, 1)
+        val targetChannelSpinner = addSpinner(panel, "Cell morphology channel 1", channel1Model)
+
+        val channel2Model = SpinnerNumberModel(0, 0, 100, 1)
+        val allCellsChannelSpinner = addSpinner(panel, "Cell morphology channel 2 (0 to disable)", channel2Model)
+
+        val transductionChannel1Model = SpinnerNumberModel(2, 1, 100, 1)
+        val transducedChannelSpinner = addSpinner(panel, "Transduction channel", transductionChannel1Model)
+
+        val imageProcessingParametersLabel = JLabel("Image processing parameters")
+        panel.add(imageProcessingParametersLabel)
+
+        // TODO: Hook this up so it actually works
+        val cellDiameterChannel1Label = JLabel("Cell diameter(px)")
+        val cellDiameterChanne1Widget = AlignedTextWidget()
+        panel.add(cellDiameterChannel1Label)
+
+        val thresholdRadiusModel = SpinnerNumberModel(20, 1, 1000, 1)
+        val thresholdRadiusSpinner = addSpinner(panel, "Local threshold radius", thresholdRadiusModel)
+
+        // TODO: Hook this up so it actually works
+        val cellDiameterChannel2Label = JLabel("Cell diameter(px)")
+        val cellDiameterChannel2Widget = AlignedTextWidget()
+        panel.add(cellDiameterChannel2Label)
+
+        val gaussianBlurModel = SpinnerNumberModel(3, 1, 50, 1)
+        val gaussianBlurSpinner = addSpinner(panel, "Gaussian blur sigma", gaussianBlurModel)
+
+        val outputParamsLabel = JLabel("Output parameters")
+        panel.add(outputParamsLabel)
+
+        val resultsOutputPanel = JPanel()
+        val resultsOutputLabel = JLabel("Results output")
+        val saveAsCSVButton = JRadioButton("Save as a CSV file")
+        val saveAsXMLButton = JRadioButton("Save as XML file")
+        val bg = ButtonGroup()
+        bg.add(saveAsCSVButton); bg.add(saveAsXMLButton)
+        resultsOutputPanel.add(resultsOutputLabel)
+        resultsOutputPanel.add(saveAsCSVButton)
+        resultsOutputPanel.add(saveAsXMLButton)
+        panel.add(resultsOutputPanel)
+
+        // TODO: Handle no file selected (when outputFile is null)
+        var outputFile: File? = null
+        val browseButton = addFileChooser(panel, "Output File (if saving)")
+        browseButton.addActionListener {
+            val fileChooser = JFileChooser()
+            fileChooser.fileSelectionMode = JFileChooser.FILES_ONLY
+            val i = fileChooser.showOpenDialog(panel)
+            if (i == JFileChooser.APPROVE_OPTION) {
+                outputFile = fileChooser.selectedFile
+            }
+        }
+
+        val okButton = JButton("Ok")
+        panel.add(okButton)
+        okButton.addActionListener {
+            // TODO: handle the null inputFolder correctly
+            val files = getAllFiles(inputFolder!!, shouldProcessFilesInNestedFoldersCheckbox.isSelected)
+            val thresholdRadius = thresholdRadiusSpinner.value as Int
+            val gaussianBlurSigma = (gaussianBlurSpinner.value as Int).toDouble()
+            val outputFormat = when {
+                saveAsCSVButton.isSelected -> OutputFormat.CSV
+                saveAsXMLButton.isSelected -> OutputFormat.XML
+                else -> // TODO: Handle no output format selected
+                    ""
+            }
+            val targetChannel = targetChannelSpinner.value as Int
+            val transducedChannel = transducedChannelSpinner.value as Int
+            val allCellsChannel = allCellsChannelSpinner.value as Int
+            val colocalizer = BatchableColocalizer(targetChannel, transducedChannel, allCellsChannel, context)
+            colocalizer.process(openFiles(files), CellDiameterRange(0.0, 100.0), thresholdRadius, gaussianBlurSigma, outputFormat, outputFile!!)
+            MessageDialog(
+                IJ.getInstance(),
+                "Saved",
+                "The batch processing results have successfully been saved to the specified file."
+            )
+        }
+
+        return panel
     }
 
     private fun gui() {
