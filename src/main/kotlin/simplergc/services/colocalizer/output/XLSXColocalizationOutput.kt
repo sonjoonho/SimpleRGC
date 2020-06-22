19467db35fbff749008d7ebd7344cabdb0a89530
Tiger Cross <tc2017@ic.ac.uk>
2020-06-22 22:16:00 +0100

    Add schema for documentation sheet

--- src/main/kotlin/simplergc/services/colocalizer/output/XLSXColocalizationOutput.kt
+++ src/main/kotlin/simplergc/services/colocalizer/output/XLSXColocalizationOutput.kt
@@ -1,6 +1,5 @@
 package simplergc.services.colocalizer.output
 
-import org.apache.poi.ss.usermodel.IndexedColors
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
 import java.io.File
 import simplergc.commands.RGCTransduction.TransductionResult
@@ -22,17 +21,39 @@ class XLSXColocalizationOutput(
         val createHelper = workbook.creationHelper
 
         // Set header font to blue and bold.
-        val headerFont = workbook.createFont()
-        headerFont.bold = true
-        headerFont.color = IndexedColors.BLUE.getIndex()
-
-        val headerCellStyle = workbook.createCellStyle()
-        headerCellStyle.setFont(headerFont)
+        // val headerFont = workbook.createFont()
+        // headerFont.bold = true
+        // headerFont.color = IndexedColors.BLUE.getIndex()
+        //
+        // val headerCellStyle = workbook.createCellStyle()
+        // headerCellStyle.setFont(headerFont)
 
         // Create sheets
         val docSheet = workbook.createSheet("Documentation")
+        val docInfo = ArrayList<Array<String>>(
+            arrayListOf(
+                arrayOf("The article: ", "TODO: Insert citation"),
+                arrayOf("", ""),
+                arrayOf("Abbreviation", "Description"),
+                arrayOf("Summary", "Key measurements per image"),
+                arrayOf("Transduced cells analysis", "Per-cell metrics of transduced cells"),
+                arrayOf("Parameters", "Parameters used to run the SimpleRGC plugin")
+            )
+        )
+        docInfo.forEachIndexed { rowIdx, rows ->
+            run {
+                val row = docSheet.createRow(rowIdx)
+                rows.forEachIndexed { colIdx, str ->
+                    run {
+                        val cell = row.createCell(colIdx)
+                        cell.setCellValue(str)
+                    }
+                }
+            }
+        }
+
         val summarySheet = workbook.createSheet("Summary")
-        val perCellAnalysisSheet = workbook.createSheet("Per-cell analysis")
+        val perCellAnalysisSheet = workbook.createSheet("Transduced cells analysis")
         val paramsSheet = workbook.createSheet("Parameters")
 
         // Write file and close streams
