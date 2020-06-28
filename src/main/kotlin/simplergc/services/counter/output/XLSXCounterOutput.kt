5c4f53be10c7081a68cb0dc88a86bdfe6ccf85cc
Will Burr <williamburr98@gmail.com>
2020-06-28 20:09:38 +0100

    Add Simple RGC Plugin and Version to XLSX Counter output

--- src/main/kotlin/simplergc/services/counter/output/XLSXCounterOutput.kt
+++ src/main/kotlin/simplergc/services/counter/output/XLSXCounterOutput.kt
@@ -22,6 +22,8 @@ class XLSXCounterOutput(
     private val columns = arrayOf(
         "File Name",
         "Cell Count",
+        "Simple RGC Plugin",
+        "Version",
         "Morphology Channel",
         "Smallest Cell Diameter (px)",
         "Largest Cell Diameter (px)",
@@ -39,7 +41,12 @@ class XLSXCounterOutput(
 
         val createHelper = workbook.creationHelper
 
-        val headerRow = sheet.createRow(0)
+        val articleRow = sheet.createRow(0)
+        articleRow.createCell(0).setCellValue("The article:")
+        articleRow.createCell(1).setCellValue("[insert full citation]")
+
+
+        val headerRow = sheet.createRow(3)
 
         // Header
         for (col in columns.indices) {
@@ -50,35 +57,43 @@ class XLSXCounterOutput(
         val numberCellStyle = workbook.createCellStyle()
         numberCellStyle.dataFormat = createHelper.createDataFormat().getFormat("#")
 
-        var rowIdx = 1
+        var rowIdx = 4
         for (pair in fileNameAndCountList) {
+            var colIdx = 0
             val row = sheet.createRow(rowIdx++)
 
             // File Name
-            row.createCell(0).setCellValue(pair.first.replace(",", ""))
+            row.createCell(colIdx++).setCellValue(pair.first.replace(",", ""))
 
             // Cell Count
-            val countCell = row.createCell(1)
+            val countCell = row.createCell(colIdx++)
             countCell.setCellValue(pair.second.toDouble())
 
+            // Plugin Information
+            val pluginNameCell = row.createCell(colIdx++)
+            pluginNameCell.setCellValue("RGC Counter")
+
+            val pluginVersionCell = row.createCell(colIdx++)
+            pluginVersionCell.setCellValue("1.0")
+
             // Parameters
-            val morphologyChannelCell = row.createCell(2)
+            val morphologyChannelCell = row.createCell(colIdx++)
             morphologyChannelCell.setCellValue(morphologyChannel.toDouble())
 
-            val smallestDiameterCell = row.createCell(3)
+            val smallestDiameterCell = row.createCell(colIdx++)
             smallestDiameterCell.setCellValue(cellDiameterRange.smallest)
 
-            val largestDiameterCell = row.createCell(4)
+            val largestDiameterCell = row.createCell(colIdx++)
             largestDiameterCell.setCellValue(cellDiameterRange.largest)
 
-            val localThresholdRadiusCell = row.createCell(5)
+            val localThresholdRadiusCell = row.createCell(colIdx++)
             localThresholdRadiusCell.setCellValue(localThresholdRadius.toDouble())
 
-            val gaussianBlurSigmaCell = row.createCell(6)
+            val gaussianBlurSigmaCell = row.createCell(colIdx)
             gaussianBlurSigmaCell.setCellValue(gaussianBlurSigma)
         }
 
-        for (i in 0..6) {
+        for (i in 0..8) {
             sheet.autoSizeColumn(i)
         }
 
