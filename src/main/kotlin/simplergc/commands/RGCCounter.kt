d05462f33194bf875e82631f4136449149e0c450
Will Burr <williamburr98@gmail.com>
2020-06-30 23:46:44 +0100

    Run formatter for RGCCounter

--- src/main/kotlin/simplergc/commands/RGCCounter.kt
+++ src/main/kotlin/simplergc/commands/RGCCounter.kt
@@ -185,12 +185,12 @@ class RGCCounter : Command, Previewable {
 
         if (outputFormat != OutputFormat.DISPLAY && outputFile == null) {
             val path = image.originalFileInfo.directory
-            val extension = when(outputFormat) {
+            val extension = when (outputFormat) {
                 OutputFormat.CSV -> "csv"
                 OutputFormat.XLSX -> "xlsx"
                 else -> throw IllegalArgumentException("Invalid output type provided")
             }
-            val name = FilenameUtils.removeExtension(image.originalFileInfo.fileName) + ".${extension}"
+            val name = FilenameUtils.removeExtension(image.originalFileInfo.fileName) + ".$extension"
             outputFile = File(path + name)
             if (!outputFile!!.createNewFile()) {
                 val dialog = GenericDialog("Warning")
