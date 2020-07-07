2d65c55c96f66c07370a587405e9d0c472edda1e
Tiger Cross <tc2017@ic.ac.uk>
2020-07-07 19:32:06 +0100

    Refactor Documentation and Summary sheet generation into functions

--- src/main/kotlin/simplergc/services/colocalizer/output/XLSXColocalizationOutput.kt
+++ src/main/kotlin/simplergc/services/colocalizer/output/XLSXColocalizationOutput.kt
@@ -1,9 +1,9 @@
 package simplergc.services.colocalizer.output
 
+import org.apache.commons.io.FilenameUtils
 import java.io.File
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
 import simplergc.commands.RGCTransduction.TransductionResult
-import simplergc.services.SimpleOutput
 
 /**
  * Displays a table for a transduction analysis with the result of
@@ -12,77 +12,16 @@ import simplergc.services.SimpleOutput
 class XLSXColocalizationOutput(
     private val result: TransductionResult,
     private val outputFile: File
-) : SimpleOutput() {
+) : ColocalizationOutput() {
 
     override fun output() {
 
         // Create Excel File.
         val workbook = XSSFWorkbook()
 
-        // Create sheets
-        val docSheet = workbook.createSheet("Documentation")
-        val docInfo = ArrayList<Array<String>>(
-            arrayListOf(
-                arrayOf("The article: ", "TODO: Insert citation"),
-                arrayOf("", ""),
-                arrayOf("Abbreviation", "Description"),
-                arrayOf("Summary", "Key measurements per image"),
-                arrayOf("Transduced cells analysis", "Per-cell metrics of transduced cells"),
-                arrayOf("Parameters", "Parameters used to run the SimpleRGC plugin")
-            )
-        )
-        docInfo.forEachIndexed { rowIdx, rows ->
-            run {
-                val row = docSheet.createRow(rowIdx)
-                rows.forEachIndexed { colIdx, str ->
-                    run {
-                        val cell = row.createCell(colIdx)
-                        cell.setCellValue(str)
-                    }
-                }
-            }
-        }
+        writeDocSheet(workbook)
 
-        // TODO: Modify following 2 sheets to work on a per file basis
-        val summarySheet = workbook.createSheet("Summary")
-        val summaryHeader = arrayOf(
-            "File Name",
-            "Number of Cells",
-            "Number of Transduced Cells",
-            "Transduction Efficiency (%)",
-            "Average Morphology Area (pixel^2)",
-            "Mean Fluorescence Intensity (a.u.)",
-            "Median Fluorescence Intensity (a.u.)",
-            "Min Fluorescence Intensity (a.u.)",
-            "Max Fluorescence Intensity (a.u.)",
-            "IntDen",
-            "RawIntDen"
-        )
-        // Create header for summary
-        val summaryHeaderRow = summarySheet.createRow(0)
-        summaryHeader.forEachIndexed { columnIdx, headerTitle ->
-            val cell = summaryHeaderRow.createCell(columnIdx)
-            cell.setCellValue(headerTitle)
-        }
-        // Add summary data
-        val summaryData = arrayOf(
-            "1",
-            result.targetCellCount.toString(),
-            result.overlappingTwoChannelCells.size.toString(),
-            ((result.overlappingTwoChannelCells.size / result.targetCellCount.toDouble()) * 100).toString(),
-            "TODO: Average Morphology Area",
-            (result.overlappingTransducedIntensityAnalysis.sumBy { it.mean } / result.overlappingTransducedIntensityAnalysis.size).toString(),
-            "TODO: Median",
-            "TODO: Min",
-            "TODO: Max",
-            "TODO: IntDen",
-            "TODO: RawIntDen"
-        )
-        val summaryDataRow = summarySheet.createRow(1)
-        summaryData.forEachIndexed { columnIdx, dataEntry ->
-            val cell = summaryDataRow.createCell(columnIdx)
-            cell.setCellValue(dataEntry)
-        }
+        writeSummarySheet(workbook)
 
         val perCellAnalysisSheet = workbook.createSheet("Transduced cells analysis")
         val perCellAnalysisHeader = arrayOf(
@@ -139,11 +78,78 @@ class XLSXColocalizationOutput(
         // TODO: insert params here
 
         // Write file and close streams
-        val outputXlsxFile =
-            if (outputFile.extension == "xlsx") outputFile else File("${outputFile.parent}${File.separator}${outputFile.nameWithoutExtension}.xlsx")
+        val outputXlsxFile = File(FilenameUtils.removeExtension(outputFile.path) + ".xlsx")
         val xlsxFileOut = outputXlsxFile.outputStream()
         workbook.write(xlsxFileOut)
         xlsxFileOut.close()
         workbook.close()
+    }
+
+    fun writeDocSheet(workbook: XSSFWorkbook) {
+        val docSheet = workbook.createSheet("Documentation")
+        val docInfo = ArrayList<Array<String>>(
+            arrayListOf(
+                arrayOf("The article: ", "TODO: Insert citation"),
+                arrayOf("", ""),
+                arrayOf("Abbreviation", "Description"),
+                arrayOf("Summary", "Key measurements per image"),
+                arrayOf("Transduced cells analysis", "Per-cell metrics of transduced cells"),
+                arrayOf("Parameters", "Parameters used to run the SimpleRGC plugin")
+            )
+        )
+        docInfo.forEachIndexed { rowIdx, rows ->
+            run {
+                val row = docSheet.createRow(rowIdx)
+                rows.forEachIndexed { colIdx, str ->
+                    run {
+                        val cell = row.createCell(colIdx)
+                        cell.setCellValue(str)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun writeSummarySheet(workbook: XSSFWorkbook) {
+        // TODO: Modify following 2 sheets to work on a per file basis
+        val summarySheet = workbook.createSheet("Summary")
+        val summaryHeader = arrayOf(
+            "File Name",
+            "Number of Cells",
+            "Number of Transduced Cells",
+            "Transduction Efficiency (%)",
+            "Average Morphology Area (pixel^2)",
+            "Mean Fluorescence Intensity (a.u.)",
+            "Median Fluorescence Intensity (a.u.)",
+            "Min Fluorescence Intensity (a.u.)",
+            "Max Fluorescence Intensity (a.u.)",
+            "RawIntDen"
+        )
+        // Create header for summary
+        val summaryHeaderRow = summarySheet.createRow(0)
+        summaryHeader.forEachIndexed { columnIdx, headerTitle ->
+            val cell = summaryHeaderRow.createCell(columnIdx)
+            cell.setCellValue(headerTitle)
+        }
+        // Add summary data
+        fileNameAndResultsList.forEachIndexed { rowIdx, fileNameAndResult ->
+            val summaryData = arrayOf(
+                fileNameAndResult.first,
+                fileNameAndResult.second.targetCellCount.toString(),
+                fileNameAndResult.second.overlappingTwoChannelCells.size.toString(),
+                ((fileNameAndResult.second.overlappingTwoChannelCells.size / fileNameAndResult.second.targetCellCount.toDouble()) * 100).toString(),
+                (fileNameAndResult.second.overlappingTransducedIntensityAnalysis.sumBy { it.area } / fileNameAndResult.second.overlappingTransducedIntensityAnalysis.size).toString(),
+                (fileNameAndResult.second.overlappingTransducedIntensityAnalysis.sumBy { it.mean } / fileNameAndResult.second.overlappingTransducedIntensityAnalysis.size).toString(),
+                (fileNameAndResult.second.overlappingTransducedIntensityAnalysis.sumBy { it.median } / fileNameAndResult.second.overlappingTransducedIntensityAnalysis.size).toString(),
+                (fileNameAndResult.second.overlappingTransducedIntensityAnalysis.sumBy { it.min } / fileNameAndResult.second.overlappingTransducedIntensityAnalysis.size).toString(),
+                (fileNameAndResult.second.overlappingTransducedIntensityAnalysis.sumBy { it.max } / fileNameAndResult.second.overlappingTransducedIntensityAnalysis.size).toString(),
+                (fileNameAndResult.second.overlappingTransducedIntensityAnalysis.sumBy { it.rawIntDen } / fileNameAndResult.second.overlappingTransducedIntensityAnalysis.size).toString()
+            )
+            val summaryDataRow = summarySheet.createRow(rowIdx)
+            summaryData.forEachIndexed { columnIdx, dataEntry ->
+                val cell = summaryDataRow.createCell(columnIdx)
+                cell.setCellValue(dataEntry)
+            }
+        }
     }
 }
