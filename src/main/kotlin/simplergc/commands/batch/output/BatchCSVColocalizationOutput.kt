9285e2a288a529f6f407fd66102f7b0afca3fb06
Tiger Cross <tc2017@ic.ac.uk>
2020-07-19 12:59:39 +0100

    Create Batch output class skeleton

---
+++ src/main/kotlin/simplergc/commands/batch/output/BatchCSVColocalizationOutput.kt
@@ -0,0 +1,11 @@
+package simplergc.commands.batch.output
+
+import simplergc.services.Parameters
+
+/**
+ * Displays a table for a transduction analysis with the result of
+ * overlapping, transduced cells.
+ */
+class BatchCSVColocalizationOutput(private val transductionParameters: Parameters.TransductionParameters) {
+
+}
---
+++ src/main/kotlin/simplergc/commands/batch/output/BatchXLSXColocalizationOutput.kt
@@ -0,0 +1,15 @@
+package simplergc.commands.batch.output
+
+import java.io.File
+import org.apache.commons.io.FilenameUtils
+import org.apache.poi.xssf.usermodel.XSSFWorkbook
+import simplergc.services.Parameters
+import simplergc.services.Table
+
+/**
+ * Displays a table for a transduction analysis with the result of
+ * overlapping, transduced cells.
+ */
+class BatchXLSXColocalizationOutput(private val transductionParameters: Parameters.TransductionParameters) {
+
+}
