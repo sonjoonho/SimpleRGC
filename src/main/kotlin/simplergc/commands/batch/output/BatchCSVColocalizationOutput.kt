a0871f642f928ce20ee654e27e3a08d39f255f27
Tiger Cross <tc2017@ic.ac.uk>
2020-07-23 12:33:25 +0100

    Add metric writing functionality for CSV

--- src/main/kotlin/simplergc/commands/batch/output/BatchCSVColocalizationOutput.kt
+++ src/main/kotlin/simplergc/commands/batch/output/BatchCSVColocalizationOutput.kt
@@ -1,6 +1,11 @@
 package simplergc.commands.batch.output
 
+import simplergc.services.BaseRow
+import simplergc.services.Field
+import simplergc.services.IntField
 import simplergc.services.Parameters.TransductionParameters
+import simplergc.services.StringField
+import simplergc.services.Table
 import simplergc.services.colocalizer.output.CSVColocalizationOutput
 import java.io.File
 
@@ -47,5 +52,62 @@ class BatchCSVColocalizationOutput(private val transductionParameters: Transduct
         documentationCsv.addRow(DocumentationRow("Raw IntDen:", "Raw Integrated Density for each transduced cell"))
         documentationCsv.addRow(DocumentationRow("Parameters: ", "Parameters used for SimpleRGC plugin"))
         documentationCsv.produceCSV(File("${outputPath}Documentation.csv"))
+    }
+
+    private val metricMappings = mapOf(
+        "Morphology Area" to fileNameAndResultsList.map {
+            Pair(
+                it.first,
+                it.second.overlappingTransducedIntensityAnalysis.map { cell -> cell.area })
+        },
+        "Mean Int" to fileNameAndResultsList.map {
+            Pair(
+                it.first,
+                it.second.overlappingTransducedIntensityAnalysis.map { cell -> cell.mean })
+        },
+        "Median Int" to fileNameAndResultsList.map {
+            Pair(
+                it.first,
+                it.second.overlappingTransducedIntensityAnalysis.map { cell -> cell.median })
+        },
+        "Min Int" to fileNameAndResultsList.map {
+            Pair(
+                it.first,
+                it.second.overlappingTransducedIntensityAnalysis.map { cell -> cell.min })
+        },
+        "Max Int" to fileNameAndResultsList.map {
+            Pair(
+                it.first,
+                it.second.overlappingTransducedIntensityAnalysis.map { cell -> cell.max })
+        },
+        "Raw IntDen" to fileNameAndResultsList.map {
+            Pair(
+                it.first,
+                it.second.overlappingTransducedIntensityAnalysis.map { cell -> cell.rawIntDen })
+        }
+
+    )
+
+    val metricData = Table(
+        arrayOf(
+            "Transduced Cell" + fileNameAndResultsList.map { it.first }.toList()
+        )
+    )
+
+    data class metricRow(val rowIdx: Int, val metrics: List<Int?>) : BaseRow {
+        override fun toList(): List<Field> {
+            val row = mutableListOf(IntField(rowIdx) as Field)
+            row.addAll(metrics.map { StringField(it?.toString() ?: "") })
+            return row.toList()
+        }
+    }
+
+    private fun writeMetricCSV(metricName: String) {
+        val maxRows =
+            fileNameAndResultsList.maxBy { it.second.overlappingTwoChannelCells.size }?.second?.overlappingTwoChannelCells?.size
+        for (rowIdx in 0..maxRows!!) {
+            val rowData = metricMappings.getOrDefault(metricName, emptyList()).map { it.second.getOrNull(rowIdx) }
+            metricData.addRow(metricRow(rowIdx, rowData))
+        }
     }
 }
