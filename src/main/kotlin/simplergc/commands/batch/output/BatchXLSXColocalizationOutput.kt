32a90631ca5c167d24b9a104025ec2d4989dd830
Tiger Cross <tc2017@ic.ac.uk>
2020-07-23 12:41:57 +0100

    Make BatchXLSXColocalizationOutput inherit from XLSXColocalizationOutput

--- src/main/kotlin/simplergc/commands/batch/output/BatchXLSXColocalizationOutput.kt
+++ src/main/kotlin/simplergc/commands/batch/output/BatchXLSXColocalizationOutput.kt
@@ -1,9 +1,11 @@
 package simplergc.commands.batch.output
 
 import simplergc.services.Parameters
+import simplergc.services.colocalizer.output.XLSXColocalizationOutput
 
 /**
  * Displays a table for a transduction analysis with the result of
  * overlapping, transduced cells.
  */
-class BatchXLSXColocalizationOutput(private val transductionParameters: Parameters.TransductionParameters)
+class BatchXLSXColocalizationOutput(private val transductionParameters: Parameters.TransductionParameters) :
+    XLSXColocalizationOutput(transductionParameters)
