cdba19c7b393f8d3f9a5cbc8265ba02481892f0a
Tiger Cross <33670466+Tiger-Cross@users.noreply.github.com>
2020-08-12 16:10:12 +0100

    Rephrase batch exception/dialog messages.

--- src/main/kotlin/simplergc/commands/batch/controllers/RGCController.kt
+++ src/main/kotlin/simplergc/commands/batch/controllers/RGCController.kt
@@ -20,11 +20,11 @@ abstract class RGCController(private val statusService: StatusService) {
 
     fun process(p: RGCParameters) {
         if (p.inputDirectory == null) {
-            throw FileNotFoundException("No input directory is selected")
+            throw FileNotFoundException("No input directory is selected.")
         } else if (p.outputFile == null) {
-            throw FileNotFoundException("No output file selected")
+            throw FileNotFoundException("No output file selected.")
         } else if (!p.inputDirectory!!.exists()) {
-            throw FileNotFoundException("The input folder could not be opened. Please create it if it does not already exist")
+            throw FileNotFoundException("The input folder could not be opened. Please create it if it does not already exist.")
         }
         val processor = makeProcessor(p)
 
@@ -49,7 +49,7 @@ abstract class RGCController(private val statusService: StatusService) {
             try {
                 p = harvestParameters()
             } catch (dpe: DiameterParseException) {
-                view.dialog("Error", dpe.message ?: "Could not parse cell diameter")
+                view.dialog("Error", dpe.message ?: "An invalid format for the cell diameter range has been entered. The cell diameter range should be entered in the format '# - #' in which # is a number (up to two decimal places).")
                 return@ActionListener
             }
 
@@ -58,13 +58,13 @@ abstract class RGCController(private val statusService: StatusService) {
                 try {
                     process(p)
                     view.close()
-                    view.dialog("Saved", "The batch processing results have successfully been saved to ${p.outputFile}")
+                    view.dialog("Saved", "The RGC Batch results have successfully been saved to ${p.outputFile}.")
                 } catch (e: FileNotFoundException) {
                     view.dialog("Error", e.message ?: "File not found.")
                 } catch (e: IOException) {
                     view.dialog("Error", e.message ?: "File could not be opened/saved.")
                 } catch (e: Exception) {
-                    view.dialog("Error", e.message ?: "An error occurred")
+                    view.dialog("Error", e.message ?: "An error occurred. Please try again.")
                 }
             }
         }
