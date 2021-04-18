5619735ce41b3ce38929f0cc1638f317f2d1b0dc
Tiger Cross <tcross123@hotmail.co.uk>
2021-04-18 21:18:03 +0100

    Implement counter aggregate abstract functions.

--- src/main/kotlin/simplergc/services/counter/output/CounterOutput.kt
+++ src/main/kotlin/simplergc/services/counter/output/CounterOutput.kt
@@ -1,12 +1,16 @@
 package simplergc.services.counter.output
 
+import simplergc.services.Aggregate
+import simplergc.services.AggregateRow
 import simplergc.services.BaseRow
 import simplergc.services.DoubleField
+import simplergc.services.Field
 import simplergc.services.HeaderField
 import simplergc.services.IntField
 import simplergc.services.Output
 import simplergc.services.StringField
 import simplergc.services.Table
+import simplergc.services.XlsxAggregateGenerator
 
 /**
  * Outputs the result of cell counting.
@@ -24,6 +28,24 @@ abstract class CounterOutput : Output {
     }
 
     protected val resultsData = Table()
+
+    // spaces is used for csv aggregate generator (to avoid formatting issues.)
+    // startRow is only used by xlsx aggregate generator
+    abstract fun generateAggregateRow(
+        aggregate: Aggregate,
+        cellCounts: List<Number>,
+        spaces: Int = 0,
+        startRow: Int = 5
+    ): AggregateRow
+
+    fun addTotalRow(t: Table, cellCounts: List<Int>) {
+        val totalRow = AggregateRow(
+            "Total",
+            listOf(IntField(cellCounts.sum())),
+            spaces = 0
+        )
+        t.addRow(totalRow)
+    }
 
     data class ResultsRow(val fileName: String, val count: Int) : BaseRow {
         override fun toList() = listOf(StringField(fileName), IntField(count))
