89ee93805be719a4c0db94eb64090ab556a4c4f0
Tiger Cross <tcross123@hotmail.co.uk>
2021-04-18 21:52:48 +0100

    Implement aggregate row generator in ImageJCounterOutput as No-op.

--- src/main/kotlin/simplergc/services/counter/output/ImageJTableCounterOutput.kt
+++ src/main/kotlin/simplergc/services/counter/output/ImageJTableCounterOutput.kt
@@ -1,6 +1,8 @@
 package simplergc.services.counter.output
 
 import org.scijava.ui.UIService
+import simplergc.services.Aggregate
+import simplergc.services.AggregateRow
 import simplergc.services.FieldRow
 import simplergc.services.HeaderField
 import simplergc.services.ImageJTableWriter
@@ -18,5 +20,14 @@ class ImageJTableCounterOutput(uiService: UIService) : CounterOutput() {
             resultsData.addRow(ResultsRow(fileName.replace(",", ""), count))
         }
         tableWriter.produce(resultsData, "")
+    }
+
+    override fun generateAggregateRow(
+        aggregate: Aggregate,
+        cellCounts: List<Number>,
+        spaces: Int,
+        startRow: Int
+    ): AggregateRow {
+        TODO("No-op. This function should only be called in RGC-Batch.")
     }
 }
