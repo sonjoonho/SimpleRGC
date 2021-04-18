c13ddeb0a2bf49c55ce0773b3ac8b826d9014813
Tiger Cross <tcross123@hotmail.co.uk>
2021-04-18 21:18:15 +0100

    Implement counter aggregate for CSV

--- src/main/kotlin/simplergc/services/counter/output/CsvCounterOutput.kt
+++ src/main/kotlin/simplergc/services/counter/output/CsvCounterOutput.kt
@@ -1,5 +1,8 @@
 package simplergc.services.counter.output
 
+import simplergc.services.Aggregate
+import simplergc.services.AggregateRow
+import simplergc.services.CsvAggregateGenerator
 import java.io.File
 import simplergc.services.CsvTableWriter
 import simplergc.services.EmptyRow
@@ -14,21 +17,41 @@ class CsvCounterOutput(private val outputFile: File, private val counterParamete
      * Saves count results into csv file at specified output path.
      */
     override fun output() {
+        val cellCounts = mutableListOf<Int>()
         resultsData.addRow(CitationRow())
         resultsData.addRow(EmptyRow())
         resultsData.addRow(FieldRow(parameterHeadings))
         for ((fileName, count) in fileNameAndCountList) {
+            cellCounts.add(count)
             resultsData.addRow(
                 ParametersResultsRow(
                     fileName = fileName.replace(",", ""),
                     cellCount = count,
                     morphologyChannel = counterParameters.targetChannel,
-                smallestCellDiameter = counterParameters.cellDiameterRange.smallest,
-                largestCellDiameter = counterParameters.cellDiameterRange.largest,
-                localThresholdRadius = counterParameters.localThresholdRadius,
-                gaussianBlurSigma = counterParameters.gaussianBlurSigma
-                ))
+                    smallestCellDiameter = counterParameters.cellDiameterRange.smallest,
+                    largestCellDiameter = counterParameters.cellDiameterRange.largest,
+                    localThresholdRadius = counterParameters.localThresholdRadius,
+                    gaussianBlurSigma = counterParameters.gaussianBlurSigma
+                )
+            )
+        }
+        addTotalRow(resultsData, cellCounts)
+        Aggregate.values().forEach {
+            resultsData.addRow(generateAggregateRow(it, cellCounts))
         }
         tableWriter.produce(resultsData, outputFile.absolutePath)
+    }
+
+    override fun generateAggregateRow(
+        aggregate: Aggregate,
+        cellCounts: List<Number>,
+        spaces: Int,
+        startRow: Int
+    ): AggregateRow {
+        return AggregateRow(
+            aggregate.abbreviation,
+            listOf(aggregate.generateValue(CsvAggregateGenerator(cellCounts))),
+            spaces
+        )
     }
 }
