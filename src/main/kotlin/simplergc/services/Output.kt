bcaffdf7bfb7b4677cdbd3a0261647b14283a46b
Joon-Ho Son <sonjoonh@gmail.com>
2021-06-28 16:30:49 +0100

    Update citation

--- src/main/kotlin/simplergc/services/Output.kt
+++ src/main/kotlin/simplergc/services/Output.kt
@@ -8,7 +8,7 @@ interface Output {
     val tableWriter: TableWriter
 
     companion object {
-        const val ARTICLE_CITATION = "https://arxiv.org/abs/2008.06276"
+        const val ARTICLE_CITATION = "Cross T, Navarange R, Son J-H, Burr W, Singh A, Zhang K, Rusu M, Gkoutzis K, Osborne A, Nieuwenhuis B 2021 Simple RGC: ImageJ Plugins for Counting Retinal Ganglion Cells and Determining the Transduction Efficiency of Viral Vectors in Retinal Wholemounts. Journal of Open Research Software, 9: 15. DOI: https://doi.org/10.5334/jors.342"
         const val PLUGIN_VERSION = "1.1.0"
     }
 
